f0dd4e756f4126f95b9a0914f14426f82ec08b9e
guannan zhang <guannanzhang@126.com>
2011-09-22 00:04:48 +0800

    GCD

--- C-Story.cpp
+++ C-Story.cpp
@@ -6,5 +6,22 @@ int main()
 	printf("#1(jianru.lin): oh yeah~!\n");
 	// your turn
 	// ...
+	printf("I don't know what to say!\n");
+	printf("I think I need write something new!\n");
+	int m,n,t=0;
+	printf("please enter two nem:");
+	scanf("%d,%d",&m,&n);
+       if(m<n)
+       {t=m;m=n;n=t}
+       else
+       {
+　　    do
+       {
+　　    r=m%n;
+　　    m=n;
+　　    n=r;
+　　   }while(r);
+       }
+　　printf("The GCD is %d !\n",m);
 	return 0;
 }
