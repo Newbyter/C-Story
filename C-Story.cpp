23f2ca329545ddec02225b7410de06257b3c2e81
Han.Lee <han.love.stone@gmail.com>
2011-09-21 23:21:19 +0800

    nonsense

--- C-Story.cpp
+++ C-Story.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+#include<iostream>
 int main()
 {
 	// 2011-9-20 by jianru.lin@gmail.com
 	printf("#1(jianru.lin): oh yeah~!\n");
 	// your turn
 	// ...
+	std::cout<<"Let's BS Mr.Lin!"<<std::endl;
+	char Lin='5',Jian='2',Ru='0';
+	std::cout<<Lin Jian Ru<<std::endl;
 	return 0;
 }
